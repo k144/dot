1333a481c0e4207d1676e14525ddaf3779859400
Jakub Nowak <jakubnowak712@gmail.com>
2019-12-07 22:53:15 +0100

    small changes

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,9 @@ abbr -a -U po poweroff
 abbr -a -U spo sudo poweroff
 abbr -a -U rb reboot
 abbr -a -U srb sudo reboot
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+#eval /home/kuba/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
